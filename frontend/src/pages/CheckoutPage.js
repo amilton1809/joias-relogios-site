369bc6295550b962b5fdbcd1b921fb5c82859524
amilton1809 <a.milton18@hotmail.com>
2025-06-05 19:44:28 -0300

    att12

--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaCreditCard, FaMoneyBill, FaBarcode } from 'react-icons/fa';
 import axios from 'axios';
 import { useAuth } from '../context/AuthContext';
+import './CheckoutPage.css'
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
 
